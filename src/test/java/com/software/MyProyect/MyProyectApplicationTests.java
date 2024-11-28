0f6082eb9ad983bc6c60b4f4ba38a1d1262214b9
SDuque28 <santiago.duquer@autonoma.edu.co>
2024-11-28 18:19:18 -0500

    Add first test

--- src/test/java/com/software/MyProyect/MyProyectApplicationTests.java
+++ src/test/java/com/software/MyProyect/MyProyectApplicationTests.java
@@ -1,4 +1,12 @@
 package com.software.MyProyect;
 
+import org.junit.jupiter.api.*;
+
 class MyProyectApplicationTests {
+
+    @Test
+    void agregarCalificacionTest() {
+        int valor = 1;
+        Assertions.assertEquals(1, valor);
+    }
 }
